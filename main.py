44fdc942aead50f9dbcda0a7917d4f4f1e4b1056
Maverick Charmer <hifex7680@gmail.com>
2025-08-30 14:30:39 +0800

    增加返回版本号，修复了延迟计算不正确的问题

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ async def read_root(timestamp: int):
     nowtimestamp = time.time()
     nowtime = datetime.fromtimestamp(nowtimestamp)
     timedelta = nowtime - datetime.fromtimestamp(timestamp)
-    ms = str(int(timedelta.total_seconds() * 1000 % 1000))
-    return {"status": "ok", "app": "jmcomic_server_api", "latency": ms}
+    ms = str(int(timedelta.microseconds / 1000))
+    return {"status": "ok", "app": "jmcomic_server_api", "latency": ms,"version": "1.0"}
 
 
 @app.get("/download/album/{album_id}")
